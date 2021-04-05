3a5b4ce65f68b0d1e0261cf29a72dda095b6b6c0
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-05 13:47:25 -0500

    Modified name of database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const app = require('./src/app');
 
 let server;
-mongoose.connect("mongodb://127.0.0.1:27017/colegio", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://127.0.0.1:27017/messagesApp", {useNewUrlParser: true, useUnifiedTopology: true})
 .then(res=>{
   console.log("Connected to mongodb!")
   server = app.listen(process.env.PORT || 3001, () => {
